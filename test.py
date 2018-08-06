64b58552c1c91ef14cd945ed8b7687f18d9b0cc0
tao.cai01 <tao.cai01@mljr.com>
2018-08-06 17:25:33 +0800

    gggg

--- test.py
+++ test.py
@@ -3,4 +3,33 @@
 @desc: 
 """
 __author__ = 'caitao'
-print("222")
+def cmp(src_data,dst_data):
+	
+	if isinstance(src_data, dict):
+		"""若为dict格式"""
+		for key,val in dst_data.items():
+			if key not in src_data:
+				print("src不存在这个key:"+key+":"+val)
+		for key in src_data:
+			if key in dst_data:
+				"""递归"""
+				cmp(src_data[key], dst_data[key])
+			else:
+				print("dst不存在这个key:"+key)
+	elif isinstance(src_data, list):
+		"""若为list格式"""
+		if len(src_data) != len(dst_data):
+			print("list len: '{}' != '{}'".format(len(src_data), len(dst_data)))
+		for src_list, dst_list in zip(sorted(src_data), sorted(dst_data)):
+			"""递归"""
+			cmp(src_list, dst_list)
+	else:
+		if str(src_data) != str(dst_data):
+			print(src_data)
+
+if __name__ == "__main__":
+	dict1 = {"id": "504","id2": "504", "name": "班级优化", "info": {"uid":"2017","stuName":["张三","李四","李四2"]}}
+	dict2 = {"id": "503","id3": "504", "name": "班级优化2", "info": {"uid":"2017","stuName":["张三","赵五"]}}
+	# for key,va in dict2.items():
+	# 	print(key,va)
+	cmp(dict1,dict2)
--- test34.py
+++ test34.py
@@ -3,3 +3,25 @@
 @desc: 
 """
 __author__ = 'caitao'
+def cmp(src_data,dst_data):
+	
+	if isinstance(src_data, dict):
+		"""若为dict格式"""
+		for key,val in dst_data.items():
+			if key not in src_data:
+				print("src不存在这个key:"+key+":"+val)
+			elif key in src_data:
+				if (dst_data[key] == src_data[key]):
+					pass
+				else:
+					print(str(key) +":"+ str(dst_data[key]))
+			else:
+				print("dst不存在这个key:"+key)
+
+
+if __name__ == "__main__":
+	dict1 = {"id": "504","id2": "504", "name": "班级优化", "info": {"uid":"2017","stuName":["张三","李四","李四2"]}}
+	dict2 = {"id": "503","id3": "504", "name": "班级优化2", "info": {"uid":"2017","stuName":["张三","赵五"]}}
+	# for key,va in dict2.items():
+	# 	print(key,va)
+	cmp(dict1,dict2)
